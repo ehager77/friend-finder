b1f7ae6a21ffdbbdfd90fb77bfed685a5580fa4b
ehager77 <eric@isabel.io>
2019-04-02 17:59:30 -0700

    added bachelorettes to friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,163 @@
+// Data structure that will store the friends data
+var bachelorettes = [
+	{
+		name: 'Bri',
+		photo: 'http://datg-cms-source-repo.s3-website-us-east-1.amazonaws.com/ABC/TheBachelor/BCH_S23_cast_BRIANNA/07b564b8-ce5f-4569-a706-5c25ff2b59ce.jpg',
+		scores: [
+			'1',
+			'5',
+			'5',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Caelynn',
+		photo: 'http://datg-cms-source-repo.s3-website-us-east-1.amazonaws.com/ABC/TheBachelor/BCH_S23_cast_CAELYNN/74fa6d5d-d160-42fc-abe2-cea49880c315.jpg',
+		scores: [
+			'2',
+			'4',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'2',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Cassie',
+		photo: 'http://datg-cms-source-repo.s3-website-us-east-1.amazonaws.com/ABC/TheBachelor/BCH_S23_cast_CASSIE/9d96d03c-cf94-490f-aaba-b9a9d199c830.jpg',
+		scores: [
+			'4',
+			'3',
+			'5',
+			'1',
+			'1',
+			'5',
+			'5',
+			'1',
+			'1',
+			'1'
+		]
+	},
+	{
+		name: 'Courtney',
+		photo: 'http://datg-cms-source-repo.s3-website-us-east-1.amazonaws.com/ABC/TheBachelor/BCH_S23_cast_COURTNEY/db52245f-0bea-4e2e-9437-d428a86d96fe.jpg',
+		scores: [
+			'5',
+			'1',
+			'2',
+			'1',
+			'3',
+			'4',
+			'1',
+			'3',
+			'3',
+			'4'
+		]
+	},
+	{
+		name: 'Demi',
+		photo: 'http://datg-cms-source-repo.s3-website-us-east-1.amazonaws.com/ABC/TheBachelor/BCH_S23_cast_DEMI/88aa153d-6ef9-4877-9dc6-fba2a65fae16.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'1'
+		]
+	},
+	{
+		name: 'Hannah B.',
+		photo: 'http://datg-cms-source-repo.s3-website-us-east-1.amazonaws.com/ABC/TheBachelor/BCH_S23_cast_HANNAH_B/b79a3720-9597-47d5-bce7-fcf35d122980.jpg',
+		scores: [
+			'1',
+			'1',
+			'5',
+			'1',
+			'3',
+			'2',
+			'5',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Hannah G.',
+		photo: 'http://datg-cms-source-repo.s3-website-us-east-1.amazonaws.com/ABC/TheBachelor/BCH_S23_cast_HANNAH_G/625a01a5-bac7-4c66-b6a0-e9165b535cf8.jpg',
+		scores: [
+			'5',
+			'4',
+			'4',
+			'2',
+			'4',
+			'4',
+			'3',
+			'2',
+			'3',
+			'3'
+		]
+	},
+	{
+		name: 'Kirpa',
+		photo: 'http://datg-cms-source-repo.s3-website-us-east-1.amazonaws.com/ABC/TheBachelor/BCH_S23_cast_KIRPA/e445882b-9ec7-48eb-8a28-ce002569c345.jpg',
+		scores: [
+			'5',
+			'2',
+			'3',
+			'1',
+			'5',
+			'1',
+			'3',
+			'1',
+			'1',
+			'5'
+		]
+	},
+	{
+		name: 'Nicole',
+		photo: 'http://datg-cms-source-repo.s3-website-us-east-1.amazonaws.com/ABC/TheBachelor/BCH_S23_cast_NICOLE/a772a2af-24e0-42e6-967a-045c9891bdbe.jpg',
+		scores: [
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5',
+			'5'
+		]
+	},
+	{
+		name: 'Tayshia',
+		photo: 'http://datg-cms-source-repo.s3-website-us-east-1.amazonaws.com/ABC/TheBachelor/BCH_S23_cast_TAYSHIA/f04d17d2-6538-4b2e-be5e-d26aea2cb1d5.jpg',
+		scores: [
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1',
+			'1'
+		]
+	}
+];
